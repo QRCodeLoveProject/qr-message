41c13314f94c9878b1addcc43f6f9a7af192204c
PROVA1 <158753837+QRCodeLoveProject@users.noreply.github.com>
2025-06-25 19:54:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // 🚀 CAMBIA SOLO QUESTA RIGA PER AGGIORNARE IL MESSAGGIO! 👇
-const currentMessage = "Io gioco a Padel, ho appena finito una lezione per migliorare 2 colpi. Domani ad esempio invece ho 2 partite poi rimango nel centro a fare festa!";
+const currentMessage = "Io gioco a Padel, ho appena finito una lezione per migliorare 2 colpi. Domani ad esempio invece ho 2 partite poi rimango nel centro a fare festa!
+Quindi staswra poi?😏";
 
 // Inizializzazione quando la pagina è caricata
 document.addEventListener("DOMContentLoaded", function() {
