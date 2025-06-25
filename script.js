525a28b48353bf00c9bea19501d1ccab6d2f8cc2
PROVA1 <158753837+QRCodeLoveProject@users.noreply.github.com>
2025-06-25 13:35:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 🚀 CAMBIA SOLO QUESTA RIGA PER AGGIORNARE IL MESSAGGIO! 👇
-const currentMessage = "Allora cosi ad occhio e a memoria direi che sei intorno ai 20 massimo 22! E non sei piccolina ina, dai suvvia... hai le stessa mia età che mi sento dentro 😂😂😂";
+const currentMessage = "Beh, allora ci ho preso! Io 36 il 5 Agosto 🫣. Cmq l'etá è solo un numero.";
 
 // Inizializzazione quando la pagina è caricata
 document.addEventListener("DOMContentLoaded", function() {
